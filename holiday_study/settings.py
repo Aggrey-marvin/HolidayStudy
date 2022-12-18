dcdb67ed548ba853e45f6ca9d38cda38dab2808c
Zijja Aggrey <zijjaaggreymarvin@gmail.com>
2022-12-18 11:17:21 +0300

    Trying to deploy on railway

--- holiday_study/settings.py
+++ holiday_study/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-vj&a&2tc)zdl93%=xsk9+od0p$%mxp=gz6qhs)_gr@9&mbbi_w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
